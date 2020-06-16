f285ad89c192f4b5b66f2e56e69ee22c988fcd0a
Brad Williams <jb.wms89@gmail.com>
2020-06-16 17:27:12 -0400

    was able to use the search bar to change the city name within the api

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 // need a new API for every new city!!
-var cityNameEl = $("#search-bar").val();
+// Create an on click event listener to run the function city search
 
-function citySearch() {
-  cityNameEl;
-}
+$("#submit-button").on("click", function () {
+  $("#search-bar").val();
+  console.log("You clicked my button!");
+  console.log($("#search-bar").val());
 
-var queryURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=fb0a218354a329c215a0e902f7297dc6";
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    $("#search-bar").val() +
+    "&appid=fb0a218354a329c215a0e902f7297dc6";
 
-$("#submit-button").on("click", citySearch);
+  // Get the value of my input and console log it
+  // Take the city name to then search within my Atlanta API
 
-// Get the value of my input and console log it
-// Take the city name to then search within my Atlanta API
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
 
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
+    // Get the value of my input and console log it
+    // Take the city name to then search within my Atlanta API
+  });
 });
-
-// Get the value of my input and console log it
-// Take the city name to then search within my Atlanta API
