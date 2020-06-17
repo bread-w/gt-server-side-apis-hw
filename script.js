91de73754b2fe968d6e078dbe3fa11055fd67b84
Brad Williams <jb.wms89@gmail.com>
2020-06-16 22:39:41 -0400

    added icons for 5 day, now adding temp and humid

--- script.js
+++ script.js
@@ -13,18 +13,36 @@ $("#submit-button").on("click", function () {
     cityName +
     "&units=imperial&appid=fb0a218354a329c215a0e902f7297dc6";
 
+  var fiveDayURL =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&units=imperial&appid=fb0a218354a329c215a0e902f7297dc6";
   // Take the city name to then search within my Atlanta API
+  var todaysDate = moment().format("(M/D/YYYY)");
+
+  var dayOneEl = moment().add(1, "days");
+  dayOneEl = dayOneEl.format("M/D/YYYY");
+
+  var dayTwoEl = moment().add(1, "days");
+  dayTwoEl = dayTwoEl.format("M/D/YYYY");
+
+  var dayThreeEl = moment().add(1, "days");
+  dayThreeEl = dayThreeEl.format("M/D/YYYY");
+
+  var dayFourEl = moment().add(1, "days");
+  dayFourEl = dayFourEl.format("M/D/YYYY");
+
+  var dayFiveEl = moment().add(1, "days");
+  dayFiveEl = dayFiveEl.format("M/D/YYYY");
 
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
-    console.log(response.wind.speed);
     var iconImg = response.weather[0].icon;
     var iconURL = "http://openweathermap.org/img/w/" + iconImg + ".png";
 
-    $("#cityDay").text(response.name);
+    $("#cityDay").text(response.name + " " + todaysDate);
     $(".img-main").attr("src", iconURL);
     $("#temp-main").text("Temperature: " + response.main.temp);
     $("#humid-main").text("Humidity: " + response.main.humidity + "%");
@@ -33,5 +51,35 @@ $("#submit-button").on("click", function () {
 
     // Get the value of my input and console log it
     // Take the city name to then search within my Atlanta API
+  });
+
+  $.ajax({
+    url: fiveDayURL,
+    method: "GET",
+  }).then(function (response) {
+
+    var dayOneIcon = response.list[3].weather[0].icon
+    var dayTwoIcon = response.list[11].weather[0].icon
+    var dayThreeIcon = response.list[19].weather[0].icon
+    var dayFourIcon = response.list[27].weather[0].icon
+    var dayFiveIcon = response.list[35].weather[0].icon
+
+    var dayOneIconURL = "http://openweathermap.org/img/w/" + dayOneIcon + ".png";
+    var dayTwoIconURL = "http://openweathermap.org/img/w/" + dayTwoIcon + ".png";
+    var dayThreeIconURL = "http://openweathermap.org/img/w/" + dayThreeIcon + ".png";
+    var dayFourIconURL = "http://openweathermap.org/img/w/" + dayFourIcon + ".png";
+    var dayFiveIconURL = "http://openweathermap.org/img/w/" + dayFiveIcon + ".png";
+
+    $("#dayOne").text(dayOneEl);
+    $("#dayTwo").text(dayTwoEl);
+    $("#dayThree").text(dayThreeEl);
+    $("#dayFour").text(dayFourEl);
+    $("#dayFive").text(dayFiveEl);
+
+    $("#dayOneImg").attr("src", dayOneIconURL);
+    $("#dayTwoImg").attr("src", dayTwoIconURL);
+    $("#dayThreeImg").attr("src", dayThreeIconURL);
+    $("#dayFourImg").attr("src", dayFourIconURL);
+    $("#dayFiveImg").attr("src", dayFiveIconURL);
   });
 });
